20e4560c63098dfaae0d717529e1c4be3d9f8ca2
Maxou <5208681+mxyns@users.noreply.github.com>
2022-03-03 19:59:31 +0100

    test - tests showing different object decoder implementation use

--- src/test/java/com/jsoniter/TestRecord.java
+++ src/test/java/com/jsoniter/TestRecord.java
@@ -1,5 +1,8 @@
 package com.jsoniter;
 
+import com.jsoniter.annotation.JsonCreator;
+import com.jsoniter.annotation.JsonProperty;
+import com.jsoniter.spi.ClassInfo;
 import junit.framework.TestCase;
 
 import java.io.IOException;
@@ -10,11 +13,9 @@ import java.lang.reflect.Modifier;
 
 public class TestRecord extends TestCase {
 
-    record TestRecord1(long field1) {
-    }
+    record TestRecord1(long field1) {}
 
     public void test_print_record_reflection_info() {
-
 
         Class<TestRecord1> clazz = TestRecord1.class;
 
@@ -58,10 +59,33 @@ public class TestRecord extends TestCase {
         }
     }
 
-
     public void test_record_error() throws IOException {
 
-        JsonIterator iter = JsonIterator.parse("{ 'field1' : 1".replace('\'', '"'));
+        JsonIterator iter = JsonIterator.parse("{ 'field1' : 1 }".replace('\'', '"'));
         iter.read(TestRecord1.class);
+    }
+
+    public void test_record_withOnlyFieldDecoder() throws IOException {
+
+        assertEquals(ReflectionDecoderFactory.create(new ClassInfo(TestRecord1.class)).getClass(), ReflectionObjectDecoder.OnlyField.class);
+
+        JsonIterator iter = JsonIterator.parse("{ 'field1' : 1 }".replace('\'', '"'));
+        iter.read(TestRecord1.class);
+    }
+
+    public void test_record_withCtorDecoder() throws IOException {
+
+        record TestRecord2(@JsonProperty long field1) {
+
+            @JsonCreator
+            TestRecord2 {}
+        }
+
+        assertEquals(ReflectionDecoderFactory.create(new ClassInfo(TestRecord2.class)).getClass(), ReflectionObjectDecoder.WithCtor.class);
+
+        JsonIterator iter = JsonIterator.parse("{ 'field1' : 1 }".replace('\'', '"'));
+        TestRecord2 record = iter.read(TestRecord2.class);
+
+        assertEquals(record.field1, 1);
     }
 }
